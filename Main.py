33c8ae1c9b57060e35a3f34864e87328b7a63156
Luka Tosic <lukat.edu@gmail.com>
2024-05-18 13:50:14 -0600

    Final printLogic

--- Main.py
+++ Main.py
@@ -1,16 +1,37 @@
+import getpass
+
 print("Player 1 please input a word.")
 
-player1_word = input("Here: ")
+player1_word = getpass.getpass("-> ")
+
 
 underline = []
+wordtrack = []
 
 for i in player1_word:
     if i == " ":
         underline.append(" ")
+        wordtrack.append(i)
         continue
 
     underline.append("_")
+    wordtrack.append(i)
 
 
-print("Your word is ")
+print("Your word is: ")
 print(''.join(underline))
+
+curr_counter = 0
+
+while curr_counter < 7:
+    print("Player 2 guess a letter.")
+    player2_letter = input("Input Letter: ")
+
+    if player2_letter in wordtrack:
+        for index,l in enumerate(wordtrack):
+            if player2_letter == l:
+                underline[index] = l
+    print(''.join(underline))
+    curr_counter += 1
+
+
