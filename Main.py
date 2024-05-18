f84f5b9c4defdb5a845752633489ee4bbe8b444b
Luka Tosic <lukat.edu@gmail.com>
2024-05-18 13:46:38 -0600

    Letter Logic

--- Main.py
+++ Main.py
@@ -1,34 +1,16 @@
-import getpass
-
 print("Player 1 please input a word.")
-player1_word = getpass.getpass("->")
+
+player1_word = input("Here: ")
 
 underline = []
-wordtrack = []
 
 for i in player1_word:
     if i == " ":
         underline.append(" ")
-        wordtrack.append(i)
         continue
 
     underline.append("_")
-    wordtrack.append(i)
 
 
-print("Your word is: ")
+print("Your word is ")
 print(''.join(underline))
-
-curr_counter = 0
-
-while curr_counter < 7:
-    print("Player 2 guess a letter.")
-    player2_letter = input("Input Letter: ")
-
-    if player2_letter in wordtrack:
-        for index,l in enumerate(wordtrack):
-            if player2_letter == l:
-                underline[index] = l
-    print(''.join(underline))
-    curr_counter += 1
-
